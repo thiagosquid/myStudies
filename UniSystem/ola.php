f133c5150fef1fb8b593429ba91e26da6c33dc89
Thiago Gomes <thiagofg.eng@gmail.com>
2021-06-21 14:59:51 -0300

    primeiras mudanças

---
+++ UniSystem/ola.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Primeiro exemplo PHP</title>    
+    </head>
+    <body>
+        <h1>Testando PHP</h1>
+        <?php
+            echo "Olá, Mundo!";
+
+
+        ?>
+
+
+
+    </body>
+</html>
