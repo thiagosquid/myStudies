d6d47b11ae9bdacc238c31801fadbcb40ad1e9b7
Thiago Gomes <thiagofg.eng@gmail.com>
2021-07-03 21:46:35 -0300

    resolução exercício da aula 17. Fiz sozinho

---
+++ Curso JS/aula17ex/modelo/script.js
@@ -0,0 +1,60 @@
+function maior(arg){
+    arg.sort(function(a, b) {
+        return a - b;
+      });
+    return arg[arg.length - 1]
+} 
+
+function menor(arg){
+    arg.sort(function(a, b) {
+        return a - b;
+      });
+    return arg[0]
+}
+
+function soma(arg){
+    let soma = 0
+    for(let i = 0; i < arg.length; i++){
+        soma += arg[i]
+    }
+    return soma
+}
+
+function media(arg){
+    let media = soma(arg) / arg.length
+    return media
+}
+
+let num = []
+
+function adc(){
+    let cont = num.length
+    let entrada = Number(document.getElementById('cxnum').value)
+    let res = document.querySelector('div#res')
+    res.innerHTML = ``
+    
+    if(num.indexOf(entrada) != -1 || entrada < 1){
+        alert("Valor inválido ou já existente")
+    } else {
+        num.push(entrada)
+        let lista = document.getElementById('selnum')
+        let cad = document.createElement('option')
+        cad.text = `Número ${num[cont]} adicionado`
+        lista.appendChild(cad)
+    }
+}
+
+function resumo(){
+    if(num.length == 0){
+        alert("Nenhum número cadastrado!")
+    } else {
+    let res = document.querySelector('div#res')
+    res.innerHTML = `Ao todo temos ${num.length} números cadastrados.<br>
+    O maior número é ${maior(num)}<br>
+    O menor valor é ${menor(num)}<br>
+    Somando todos os valores temos ${soma(num)} <br>
+    A média dos valores digitados é ${media(num)}
+    `
+    }
+
+}
---
+++ Curso JS/teste.js
@@ -0,0 +1,6 @@
+var teste = [3,45,65,12,5,2]
+teste.sort(function(a, b) {
+    return a - b;
+  });
+
+console.log(teste)
