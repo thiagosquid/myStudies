36e9af3284a0e6fd0238d5d12a677a542b2af0ab
Thiago Gomes <thiagofg.eng@gmail.com>
2021-07-12 23:01:38 -0300

    melhorias no código

--- CFB Cursos/Curso JavaScript/Aula 50 - Jogo da Velha/script.js
+++ CFB Cursos/Curso JavaScript/Aula 50 - Jogo da Velha/script.js
@@ -1,35 +1,74 @@
-var palavras = ['banana','carro','computador','bola','brinquedo','sapato'];
-var sorteio = Math.floor(Math.random()*(palavras.length));
-sorteio = 0 //APENAS PARA FACILITAR A CONSTRUÇÃO APAGAR DEPOIS
-var palavraSel = palavras[sorteio]; //armazena a palavra sorteada
-var tamPalavra = palavraSel.length; //armazena o tamanho dessa palavra
-var espaços = []; //solução para impressão dos espaços no log
+var palavras = ['banana','carro','computador','bola','brinquedo','sapato','maçã'];
+var palavraSel = [];
+var iniciar = document.getElementById('btIniciar');
+iniciar.addEventListener('click',inicia);
+var tamPalavra;
+var espaços;
+var mostra = document.getElementById('palavraIncompleta')
+var chances;
 var letra;
-var chances = 5;
+var acerto = 0;
 
-for(var i = 0; i < tamPalavra; i++){
-    espaços[i] = '_'
+var botoes = document.body.querySelectorAll(".btLetra");
+
+function inicia(){
+    var sorteio = Math.floor(Math.random()*(palavras.length));
+    palavraSel = palavras[sorteio];
+    tamPalavra = palavraSel.length;
+    espaços = [];
+    chances = 5;
+    exibirPalavra(tamPalavra);
+    console.log(`A palavra é ${palavraSel}`)
 }
 
-console.log(espaços)
-console.log(`A palavra tem ${tamPalavra} letras`)
+function exibirPalavra(tam){
+    var impr = '';
+    for(var i = 0; i < tam; i++){
+        espaços[i] = '_'
+        impr += espaços[i];
+    }
+    mostra.innerHTML = impr;
+    pegaLetra();
+}
 
-letra = 'a';
+function pegaLetra(){
+    for(var x=0; x<botoes.length; x++){
+        botoes[x].addEventListener("click", function(){
+            document.body.querySelector("#letraSel").value = this.value;
+            letra = this.value;  
+            if(chances > 0){
+                verificaLetra();
+                }
+        });
+    } 
+}
 
-while(chances > 0){
-    if(palavraSel.indexOf(letra) > -1){
-        for(var i = palavraSel.indexOf(letra); i < tamPalavra; i++){
+
+function verificaLetra(){
+    
+    for(var i = 0; i < tamPalavra; i++){
             if(letra == palavraSel[i]){
                 espaços[i] = letra;
+                acerto = 1;                    
             }
-        }
-    }else{
-        chances -= 1;
     }
-    chances -= 1;
-    letra = 'b'
+        if(acerto == 0){
+            chances -= 1;
+        }else if(acerto ==1){
+            var impr = '';
+            for(var i = 0; i < tamPalavra; i++){
+                impr += espaços[i];
+            }
+            mostra.innerHTML = impr;
+            console.log(chances);
+            pegaLetra();
+        }else{
+            alert('acabou');
+        }
 }
-console.log(espaços);
+
+
+
 
 
 
