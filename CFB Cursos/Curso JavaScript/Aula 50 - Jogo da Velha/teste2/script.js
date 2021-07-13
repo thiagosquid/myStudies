926b8e14909a20f3cd9aca4786d12e536d521f16
Thiago Gomes <thiagofg.eng@gmail.com>
2021-07-13 20:00:36 -0300

    validação de letra repetida

--- CFB Cursos/Curso JavaScript/Aula 50 - Jogo da Velha/teste2/script.js
+++ CFB Cursos/Curso JavaScript/Aula 50 - Jogo da Velha/teste2/script.js
@@ -2,11 +2,12 @@ let lista = ['presunto', 'computador', 'carro', 'bicicleta','bola','sapato'];
 let palavraSel;
 let palavraOculta;
 let tamPal;
-let spaces = [];
-
+let espacos = [];
+let chances;
 let letreiro = document.getElementById('impr');
 let inicia = document.getElementById('btInicia');
 let letra='';
+let letrasEscolhidas = [];
 
 inicia.addEventListener('click', recebePalavra);
 
@@ -17,24 +18,33 @@ let botoes = document.body.querySelectorAll(".word");
 for(let x=0; x<botoes.length; x++){
    botoes[x].addEventListener("click", function(){
     letra = this.value;
-    verificaLetra(letra);
+        if(letrasEscolhidas.indexOf(letra) == -1){
+            if(chances > 0){
+            verificaLetra(letra);
+            }else{
+                alert('Você perdeu');
+                if(confirm('Deseja jogar novamente?')){
+                    recebePalavra();
+                }
+            }
+        }else{alert('Letra já escolhida, digite outra')}
    });
 }
-
+    //Inicialização do jogo
 function recebePalavra(){
     let impr = '';
+    chances = 5;
     palavraSel = Math.floor(Math.random()*lista.length);
     tamPal = lista[palavraSel].length;
     palavraOculta = lista[palavraSel];
     console.log(`A palavra selecionada é ${palavraOculta}`);
     for(var i = 0; i < tamPal; i++){
-        spaces[i] = '_';
-        impr += spaces[i];
+        espacos[i] = '_';
+        impr += espacos[i];
     }
     imprimeTexto(impr)
 }
-
-
+    //impressão da palavra no mostrador
 function imprimeTexto(pal){
     var upper = pal.toUpperCase();
     letreiro.innerHTML = upper;
@@ -43,18 +53,23 @@ function imprimeTexto(pal){
 function verificaLetra(l){
     let cont = 0;
     var imp = '';    
+    letrasEscolhidas.push(l);
         for(cont = 0; cont < tamPal; cont++){
             if(l == palavraOculta[cont]){
-                spaces[cont] = l;
+                espacos[cont] = l;
             }
         }
+    if(palavraOculta.indexOf(l) == -1){
+        chances -= 1;
+    }
     alteraImpressao();
 }
 
 function alteraImpressao(){
     impr = '';
     for(var i = 0; i < tamPal; i++){
-        impr += spaces[i];
+        impr += espacos[i];
     }
     imprimeTexto(impr)
 }
+
